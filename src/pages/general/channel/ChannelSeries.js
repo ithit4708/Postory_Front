d61a7b5544fea6fb6c81e1074c8d69275f932aff
dancingKim <rightlightfg@gmail.com>
2023-07-03 23:48:55 +0900

    feat: Added channel home
    1. 채널 홈 구조 작업(ChannelTemplate)
    2. 2. 채널 홈 상단 부분 채널 정보 프론트 작업
    3. 홈 탭 포스트 작업(갤러리형, 리스트형)
    4. 포스트 탭 작업(갤러리형, 리스트형)

---
+++ src/pages/general/channel/ChannelSeries.js
@@ -0,0 +1,9 @@
+import ChannelTemplate from '../../../components/templates/general/ChannelTemplate';
+
+export default function ChannelSeries() {
+  return (
+    <ChannelTemplate>
+      <div>시리즈 준비중 입니다...</div>
+    </ChannelTemplate>
+  );
+}

5865ea8c4ae9aafe4dc7072ed3eceae3d3a089e0
dancingKim <rightlightfg@gmail.com>
2023-07-07 15:13:54 +0900

    fix: fixed uploding image response url
    1. 이미지 업로드후 resonse 응답으로 오는 데이터를 고침

--- src/pages/general/create/PostCreate.js
+++ src/pages/general/create/PostCreate.js
@@ -77,11 +77,24 @@ export default function PostCreate() {
   const imgRef = useRef();
   const quillRef = useRef();
   const {
+    res: postRes,
+    error: postErr,
+    setError: setPostErr,
+    postData: post,
+  } = useApiPost(`post/create`, {
+    postType: postType,
+    postTtl: title,
+    postSbTtl: subTitle,
+    postContent: content,
+    postThumnPath: thumbnailImageUrl,
+  });
+
+  const {
     res: uploadRes,
     error: uploadErr,
     setError: setUploadErr,
     postData: upload,
-  } = useApiPost(`/uploadFiles`, new FormData());
+  } = useApiPost(`file/uploadFiles`, new FormData());
 
   const imageHandler = () => {
     console.log('에디터에서 이미지 버튼을 클릭하면 이 핸들러가 시작됩니다!');
@@ -111,31 +124,33 @@ export default function PostCreate() {
         // const result = await axios.post('http://localhost:4050/img', formData);
 
         console.log('성공 시, 백엔드가 보내주는 데이터', uploadRes.data.url);
-        let IMG_URL = {};
-        IMG_URL.add(uploadRes.data.url);
-    //     // 이 URL을 img 태그의 src에 넣은 요소를 현재 에디터의 커서에 넣어주면 에디터 내에서 이미지가 나타난다
-    //     // src가 base64가 아닌 짧은 URL이기 때문에 데이터베이스에 에디터의 전체 글 내용을 저장할 수있게된다
-    //     // 이미지는 꼭 로컬 백엔드 uploads 폴더가 아닌 다른 곳에 저장해 URL로 사용하면된다.
-    //
-    //     // 이미지 태그를 에디터에 써주기 - 여러 방법이 있다.
+        //     // 이 URL을 img 태그의 src에 넣은 요소를 현재 에디터의 커서에 넣어주면 에디터 내에서 이미지가 나타난다
+        //     // src가 base64가 아닌 짧은 URL이기 때문에 데이터베이스에 에디터의 전체 글 내용을 저장할 수있게된다
+        //     // 이미지는 꼭 로컬 백엔드 uploads 폴더가 아닌 다른 곳에 저장해 URL로 사용하면된다.
+        //
+        //     // 이미지 태그를 에디터에 써주기 - 여러 방법이 있다.
         const editor = quillRef.current.getEditor(); // 에디터 객체 가져오기
-    //     // 1. 에디터 root의 innerHTML을 수정해주기
-    //     // editor의 root는 에디터 컨텐츠들이 담겨있다. 거기에 img태그를 추가해준다.
-    //     // 이미지를 업로드하면 -> 멀터에서 이미지 경로 URL을 받아와 -> 이미지 요소로 만들어 에디터 안에 넣어준다.
-        editor.root.innerHTML =
-          editor.root.innerHTML + `<img src=${IMG_URL} /><br/>`; // 현재 있는 내용들 뒤에 써줘야한다.
-    //
-    //     // 2. 현재 에디터 커서 위치값을 가져온다
-        const range = editor.getSelection();
-    //     // 가져온 위치에 이미지를 삽입한다
-        editor.insertEmbed(range.index, 'image', IMG_URL);
+        //     // 1. 에디터 root의 innerHTML을 수정해주기
+        //     // editor의 root는 에디터 컨텐츠들이 담겨있다. 거기에 img태그를 추가해준다.
+        //     // 이미지를 업로드하면 -> 멀터에서 이미지 경로 URL을 받아와 -> 이미지 요소로 만들어 에디터 안에 넣어준다.
+        uploadRes.data.data.urls.forEach((IMG_URL) => {
+            editor.root.innerHTML =
+              editor.root.innerHTML + `<img src=${process.env.PUBLIC_URL+IMG_URL} '/><br/>`;
+            // 현재 있는 내용들 뒤에 써줘야한다.
+            //
+            //     // 2. 현재 에디터 커서 위치값을 가져온다
+            const range = editor.getSelection();
+          editor.insertEmbed(range.index, 'image', IMG_URL);
+            //     // 가져온 위치에 이미지를 삽입한다
+          }
+        )
       } catch (error) {
         console.log('실패했어요ㅠ');
       }
     });
   };
 
-// 이미지 업로드 input의 onChange
+  // 이미지 업로드 input의 onChange
   const saveImgFile = () => {
     if(imgRef.current.files.length > 0) {
       const file = imgRef.current.files[0];
@@ -150,7 +165,7 @@ export default function PostCreate() {
     }
   };
 
-// 업로드 된 이미지 미리보기
+  // 업로드 된 이미지 미리보기
   // const modules = {
   //   toolbar: [
   //     ['bold', 'italic', 'underline', 'strike'], // 텍스트 스타일
@@ -162,7 +177,7 @@ export default function PostCreate() {
   //   ],
   // };
 
- const modules = useMemo(() => {
+  const modules = useMemo(() => {
     return {
       toolbar: {
         container: [
@@ -182,13 +197,13 @@ export default function PostCreate() {
     }
   },[]);
 
-    const formats = [
-      'header',
-      'bold', 'italic', 'underline', 'strike', 'blockquote',
-      'align', 'list', 'bullet', 'indent',
-      'link', 'image',
-      'size'
-    ]
+  const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'align', 'list', 'bullet', 'indent',
+    'link', 'image',
+    'size'
+  ]
 
 
 
@@ -237,7 +252,7 @@ export default function PostCreate() {
         <form onSubmit={handleFormSubmit}>
           <h3 style={{ marginBottom : '30px' }}>포스트 작성하기</h3>
           <ImageContainerSC>
-              <PostImg src={imgFile} onClick={() => imgRef.current.click()} />
+            <PostImg src={imgFile} onClick={() => imgRef.current.click()} />
             <PostImgInputSC
               type={"file"}
               accept={"image/*"}
@@ -255,7 +270,17 @@ export default function PostCreate() {
             selectedOption={postType}
             handleOptionChange={handleOptionChange}
           />
+
           <div style={{ margin: '20px'}}></div>
+          <TextInputSC
+            type="text"
+            placeholder="제목"
+            value={title}
+            onChange={handleTitleChange}
+            onKeyDown={handleTitleChange}
+          />
+          <div style={{ margin: '20px'}}></div>
+
 
           <TextInputSC
             type="text"
@@ -265,18 +290,17 @@ export default function PostCreate() {
             onKeyDown={handleSubTitleChange}
           />
 
-          <div style={{ margin: '20px'}}></div>
           <div style={{ position: 'relative'}}>
 
-          {/*  <FontAwesomeIcon icon={faImage} style={{position: 'absolute', top: '12px', left: '5px'}}/>*/}
-          {/*  <TextInputSC*/}
-          {/*    style={{ paddingLeft: '24px'}}*/}
-          {/*    type="text"*/}
-          {/*    placeholder="썸네일 이미지 url"*/}
-          {/*    value={thumbnailImageUrl}*/}
-          {/*    onChange={handleThumbnailImageUrlChange}*/}
-          {/*    onKeyDown={handleThumbnailImageUrlChange}*/}
-          {/*  />*/}
+            {/*  <FontAwesomeIcon icon={faImage} style={{position: 'absolute', top: '12px', left: '5px'}}/>*/}
+            {/*  <TextInputSC*/}
+            {/*    style={{ paddingLeft: '24px'}}*/}
+            {/*    type="text"*/}
+            {/*    placeholder="썸네일 이미지 url"*/}
+            {/*    value={thumbnailImageUrl}*/}
+            {/*    onChange={handleThumbnailImageUrlChange}*/}
+            {/*    onKeyDown={handleThumbnailImageUrlChange}*/}
+            {/*  />*/}
           </div>
 
           <div style={{ height: '500px', margin: '20px 0 50px 0' }}>
@@ -284,7 +308,7 @@ export default function PostCreate() {
           </div>
 
           <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <SubmitButton type="submit">게시</SubmitButton>
+            <SubmitButton type="submit">게시</SubmitButton>
           </div>
         </form>
       </div>

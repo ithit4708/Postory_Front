ae4c8e86d4a528fbcdf3ea140231acf42a101136
dancingKim <rightlightfg@gmail.com>
2023-07-18 23:37:21 +0900

    update: 배포 환경에 맞게 요청 url 변경

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  // baseURL: 'http://localhost:8080',
+  baseURL: 'http://3.39.6.61:8080',
+
 });
 
 api.interceptors.request.use(

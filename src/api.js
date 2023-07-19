ac604daab1410b858f1e3bdb8048a0431995a600
dancingKim <rightlightfg@gmail.com>
2023-07-19 10:18:21 +0900

    배포 안 할시 api 주소

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  // baseURL: 'http://localhost:8080',
-  baseURL: 'http://3.39.6.61:8080',
+  baseURL: 'http://localhost:8080',
+  // baseURL: 'http://3.39.6.61:8080',
 
 });
 
